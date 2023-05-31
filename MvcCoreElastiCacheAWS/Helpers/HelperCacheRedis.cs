c4fc7f95364fa7b062e4ac4d577957f557a62751
serraguti <pacoserranox@gmail.com>
2023-05-31 11:53:08 +0200

    Add project files.

---
+++ MvcCoreElastiCacheAWS/Helpers/HelperCacheRedis.cs
@@ -0,0 +1,19 @@
+﻿using StackExchange.Redis;
+
+namespace MvcCoreElastiCacheAWS.Helpers
+{
+    public class HelperCacheRedis
+    {
+        private static Lazy<ConnectionMultiplexer> CreateConnection =
+            new Lazy<ConnectionMultiplexer>(() =>
+            {
+                //AQUI ES DONDE IRA LA CADENA DE CONEXION
+                return ConnectionMultiplexer.Connect("cache-coches.s79zfv.ng.0001.use1.cache.amazonaws.com:6379");
+            });
+
+        public static ConnectionMultiplexer Connection
+        {
+            get { return CreateConnection.Value; }
+        }
+    }
+}
